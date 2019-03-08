1aeb170ce2f38719a1de991f5ff23587e6722855
Paretkf <paretk7@gmail.com>
2019-03-08 22:39:22 +0700

    ตัดเกรด

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,13 @@
 <template>
   <div id="app">
-    <h1>BMI</h1>
-    <div>
-      ส่วนสูง : <input type="number" v-model="height" min="0"> <br>
-      น้ำหนัก : <input type="number" v-model="weight"> <br>
-      เพศ : ชาย <input type="radio" v-model="sex" value="ชาย"> หญิง <input type="radio" v-model="sex" value="หญิง"> <br>
-      อายุ : <input type="number" v-model="age">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/page3">Page3</router-link>
     </div>
-    <hr>
-    <div>
-      <h1>Result</h1>
-      <b>ส่วนสูง</b>: {{height}} <br>
-      <b>น้ำหนัก</b> : {{weight}} <br>
-      <b>เพศ</b> : {{sex}} <br>
-      <b>อายุ</b> : {{age}} <br>
-      <hr>
-      <h1>bmi</h1>
-      <h2>{{bmi}} : {{result}}</h2>
-    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-
-export default {
-  name: 'app',
-  data () {
-    return {
-      height: 0,
-      weight: 0,
-      sex: 'ชาย',
-      age: 0
-    }
-  },
-  computed: {
-    bmi () {
-      const result = this.weight/(this.height/100 * this.height/100)
-      if ((this.height/100 * this.height/100) == 0) {
-        return 0
-      }
-      return result.toFixed(2)
-    },
-    result () {
-      if (this.bmi < 18.5) {
-        return 'น้ำหนักน้อย'
-      } else if (this.bmi < 25) {
-        return 'น้ำหนักปกติ'
-      } else if (this.bmi < 30) {
-        return 'น้ำหนักเกิน'
-      } else if (this.bmi < 40) {
-        return 'โรคอ้วน'
-      }
-      return 'เป็นโรคอ้วนรักษายาก'
-    }
-  }
-}
-</script>
 
 <style>
 #app {
---
+++ src/views/Home.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="home">
+    <h1>This is home.</h1>
+    <hr>
+    จำนวนวิชา : <input type="number" v-model="amoutSubj">
+    <button @click="createMockupData()">ตกลง</button> <br>
+    <div v-for="(sub, index) in registedSubject" :key="index">
+      <select v-model="registedSubject[index]">
+        <option :value="s" v-for="s in subj" :key="s.name">{{s.name}} • {{s.credit}}</option>
+      </select>
+
+      <select v-model="registedSubject[index].grade">
+        <option :value="g" v-for="g in grade" :key="g">{{g}}</option>
+      </select>
+    </div>
+    {{registedSubject}}
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'home',
+  data () {
+    return {
+      amoutSubj: 0,
+      subj: [
+        {
+          name: 'OOP',
+          credit: 3
+        },
+        {
+          name: 'JAV',
+          credit: 3
+        },
+        {
+          name: 'STAT',
+          credit: 1
+        },
+        {
+          name: 'DATA COM',
+          credit: 1
+        }
+      ],
+      grade: ['A', 'B+', 'B', 'C+', 'C', 'D+', 'D', 'F'],
+      registedSubject: []
+    }
+  },
+  methods: {
+    createMockupData () {
+      for (let i = 0; i < this.amoutSubj; i++) {
+        this.registedSubject.push({
+          name: '',
+          credit: 0,
+          grade: ''
+        })
+      }
+    }
+  }
+}
+</script>
